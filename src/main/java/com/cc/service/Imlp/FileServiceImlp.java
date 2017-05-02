5165d828bbb178594ae44e2c7cf2d39e2656e936
FerrariCalifornia <598855214@qq.com>
2017-05-02 21:32:15 +0800

    修改完成等待测试

--- src/main/java/com/cc/service/Imlp/FileServiceImlp.java
+++ src/main/java/com/cc/service/Imlp/FileServiceImlp.java
@@ -86,120 +86,103 @@ public class FileServiceImlp implements FileService {
         String [] uploadFilesNames = uploadFiles.list();
         //String userName = uploadFilesNames[0].substring(0, uploadFilesNames[0].lastIndexOf('.') - 14);
         int uploadFilesSize = uploadFilesNames.length;
-        /** 将文件名的后缀名去掉，并且将文件名中的用户名去掉，只留下时间字符串 */
-        for(int i = 0;i < uploadFilesSize;i ++){
-            if ((uploadFilesNames[i] != null) && (uploadFilesNames[i].length() > 0)) {
-                int dot = uploadFilesNames[i].lastIndexOf('.');
-                if ((dot >-1) && (dot < (uploadFilesNames[i].length()))) {
-                    uploadFilesNames[i] =  uploadFilesNames[i].substring(0, dot);
-                    int fileNameSize = uploadFilesNames[i].length();
-                    int dotAgain = fileNameSize - 14;
-                    uploadFilesNames[i] =  uploadFilesNames[i].substring(dotAgain,fileNameSize);
+
+        //当文件夹中的文件数不为0的时候才做拼接
+        if(uploadFilesSize != 0){
+            //取到文件夹中每一个csv文件，拼接到data_train.csv后面
+            for(int i = 0;i < uploadFilesSize;i ++){
+                //得到每一个csv文件全路径
+                String inputFileNameString = train_data_path + uploadFilesNamesOrign[i];
+
+                //将用户上传的csv文件补全
+                //读取用户新上传的csv文件(按照utf-8格式来读)
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(inputFileNameString);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-            }
-        }
-
-        /** 得到文件夹中最大的文件名--即最近上传的文件 */
-        int lastUploadFileNum = 0;
-        long lastUploadFile = Long.parseLong(uploadFilesNames[0]);
-        for(int i = 0;i < uploadFilesSize;i ++){
-            if(Long.parseLong(uploadFilesNames[i]) >= lastUploadFile){
-                lastUploadFile = Long.parseLong(uploadFilesNames[i]);
-                lastUploadFileNum = i;
-            }
-        }
-
-        //得到最近上传文件的路径
-        String inputFileNameString = train_data_path + uploadFilesNamesOrign[lastUploadFileNum];
-        //String inputFileNameString = "e:\\shiptest\\nfile\\" + String.valueOf(lastUploadFile) + ".csv";
-
-
-        //将用户上传的csv文件补全
-        //读取用户新上传的csv文件(按照utf-8格式来读)
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(inputFileNameString);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        InputStreamReader isr = null;
-        try {
-            isr = new InputStreamReader(fis, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        BufferedReader br = new BufferedReader(isr);
-
-        //按行读取csv文件内容
-        String line = null;
-        //存储读取到的csv文件内容
-        String FileContent = "";
-        //计数器(用户上传的csv文件第一行的头文件不需要写到data_train.csv中，只需要写入数据行)
-        int LineNumber = 1;
-        //使用当前时间(YYYYMMDDHHMMSS)作为编号
-        Date date=new Date();
-        DateFormat format=new SimpleDateFormat("yyyyMMddHHmmss");
-        String shipIDStr=format.format(date);
-        //同一个用户文件中的编号值依次增加1
-        long shipID = Long.parseLong(shipIDStr);
-
-        try{
-            while ((line = br.readLine()) != null) {
-                //给每一行补上编号
-                if(LineNumber != 1){
-                    FileContent += String.valueOf(shipID) + ",";
-                    FileContent += line;
-                    FileContent += "\r\n"; // 补上换行符
+                InputStreamReader isr = null;
+                try {
+                    isr = new InputStreamReader(fis, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-                LineNumber++;
-                shipID++;
+                BufferedReader br = new BufferedReader(isr);
+
+                //按行读取csv文件内容
+                String line = null;
+                //存储读取到的csv文件内容
+                String FileContent = "";
+                //计数器(用户上传的csv文件第一行的头文件不需要写到data_train.csv中，只需要写入数据行)
+                int LineNumber = 1;
+                //使用当前时间(YYYYMMDDHHMMSS)作为编号
+                Date date=new Date();
+                DateFormat format=new SimpleDateFormat("yyyyMMddHHmmss");
+                String shipIDStr=format.format(date);
+                //同一个用户文件中的编号值依次增加1
+                long shipID = Long.parseLong(shipIDStr);
+
+                try{
+                    while ((line = br.readLine()) != null) {
+                        //给每一行补上编号
+                        if(LineNumber != 1){
+                            FileContent += String.valueOf(shipID) + ",";
+                            FileContent += line;
+                            FileContent += "\r\n"; // 补上换行符
+                        }
+                        LineNumber++;
+                        shipID++;
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                System.out.println(FileContent);
+
+                //将读取到的用户上传csv文件的内容续写到data_train.csv中(按照utf-8格式)
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(data_train_csv_path,true);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                OutputStreamWriter osw = null;
+                try {
+                    osw = new OutputStreamWriter(fos, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    osw.write(FileContent);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    osw.flush();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                //关闭文件流
+                try {
+                    fis.close();
+                    isr.close();
+                    fos.close();
+                    osw.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
             }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        System.out.println(FileContent);
 
-
-        //将读取到的用户上传csv文件的内容续写到data_train.csv中(按照utf-8格式)
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(data_train_csv_path,true);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        OutputStreamWriter osw = null;
-        try {
-            osw = new OutputStreamWriter(fos, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            osw.write(FileContent);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            osw.flush();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        //关闭文件流
-        try {
-            fis.close();
-            isr.close();
-            fos.close();
-            osw.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         }
     }
 
@@ -224,120 +207,103 @@ public class FileServiceImlp implements FileService {
         String [] uploadFilesNames = uploadFiles.list();
         //String userName = uploadFilesNames[0].substring(0, uploadFilesNames[0].lastIndexOf('.') - 14);
         int uploadFilesSize = uploadFilesNames.length;
-        /** 将文件名的后缀名去掉，并且将文件名中的用户名去掉，只留下时间字符串 */
-        for(int i = 0;i < uploadFilesSize;i ++){
-            if ((uploadFilesNames[i] != null) && (uploadFilesNames[i].length() > 0)) {
-                int dot = uploadFilesNames[i].lastIndexOf('.');
-                if ((dot >-1) && (dot < (uploadFilesNames[i].length()))) {
-                    uploadFilesNames[i] =  uploadFilesNames[i].substring(0, dot);
-                    int fileNameSize = uploadFilesNames[i].length();
-                    int dotAgain = fileNameSize - 14;
-                    uploadFilesNames[i] =  uploadFilesNames[i].substring(dotAgain,fileNameSize);
+
+        //当文件夹中的文件数不为0的时候才做拼接
+        if(uploadFilesSize != 0){
+            //取到文件夹中每一个csv文件，拼接到data_train.csv后面
+            for(int i = 0;i < uploadFilesSize;i ++){
+                //得到每一个csv文件全路径
+                String inputFileNameString = train_data_path + uploadFilesNamesOrign[i];
+
+                //将用户上传的csv文件补全
+                //读取用户新上传的csv文件(按照utf-8格式来读)
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(inputFileNameString);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-            }
-        }
-
-        /** 得到文件夹中最大的文件名--即最近上传的文件 */
-        int lastUploadFileNum = 0;
-        long lastUploadFile = Long.parseLong(uploadFilesNames[0]);
-        for(int i = 0;i < uploadFilesSize;i ++){
-            if(Long.parseLong(uploadFilesNames[i]) >= lastUploadFile){
-                lastUploadFile = Long.parseLong(uploadFilesNames[i]);
-                lastUploadFileNum = i;
-            }
-        }
-
-        //得到最近上传文件的路径
-        String inputFileNameString = train_data_path + uploadFilesNamesOrign[lastUploadFileNum];
-        //String inputFileNameString = "e:\\shiptest\\nfile\\" + String.valueOf(lastUploadFile) + ".csv";
-
-
-        //将用户上传的csv文件补全
-        //读取用户新上传的csv文件(按照utf-8格式来读)
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(inputFileNameString);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        InputStreamReader isr = null;
-        try {
-            isr = new InputStreamReader(fis, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        BufferedReader br = new BufferedReader(isr);
-
-        //按行读取csv文件内容
-        String line = null;
-        //存储读取到的csv文件内容
-        String FileContent = "";
-        //计数器(用户上传的csv文件第一行的头文件不需要写到data_train.csv中，只需要写入数据行)
-        int LineNumber = 1;
-        //使用当前时间(YYYYMMDDHHMMSS)作为编号
-        Date date=new Date();
-        DateFormat format=new SimpleDateFormat("yyyyMMddHHmmss");
-        String shipIDStr=format.format(date);
-        //同一个用户文件中的编号值依次增加1
-        long shipID = Long.parseLong(shipIDStr);
-
-        try{
-            while ((line = br.readLine()) != null) {
-                //给每一行补上编号
-                if(LineNumber != 1){
-                    FileContent += String.valueOf(shipID) + ",";
-                    FileContent += line;
-                    FileContent += "\r\n"; // 补上换行符
+                InputStreamReader isr = null;
+                try {
+                    isr = new InputStreamReader(fis, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-                LineNumber++;
-                shipID++;
+                BufferedReader br = new BufferedReader(isr);
+
+                //按行读取csv文件内容
+                String line = null;
+                //存储读取到的csv文件内容
+                String FileContent = "";
+                //计数器(用户上传的csv文件第一行的头文件不需要写到data_train.csv中，只需要写入数据行)
+                int LineNumber = 1;
+                //使用当前时间(YYYYMMDDHHMMSS)作为编号
+                Date date=new Date();
+                DateFormat format=new SimpleDateFormat("yyyyMMddHHmmss");
+                String shipIDStr=format.format(date);
+                //同一个用户文件中的编号值依次增加1
+                long shipID = Long.parseLong(shipIDStr);
+
+                try{
+                    while ((line = br.readLine()) != null) {
+                        //给每一行补上编号
+                        if(LineNumber != 1){
+                            FileContent += String.valueOf(shipID) + ",";
+                            FileContent += line;
+                            FileContent += "\r\n"; // 补上换行符
+                        }
+                        LineNumber++;
+                        shipID++;
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                System.out.println(FileContent);
+
+                //将读取到的用户上传csv文件的内容续写到data_train.csv中(按照utf-8格式)
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(data_train_csv_path,true);
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                OutputStreamWriter osw = null;
+                try {
+                    osw = new OutputStreamWriter(fos, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    osw.write(FileContent);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    osw.flush();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                //关闭文件流
+                try {
+                    fis.close();
+                    isr.close();
+                    fos.close();
+                    osw.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
             }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        System.out.println(FileContent);
 
-
-        //将读取到的用户上传csv文件的内容续写到data_train.csv中(按照utf-8格式)
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(data_train_csv_path,true);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        OutputStreamWriter osw = null;
-        try {
-            osw = new OutputStreamWriter(fos, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            osw.write(FileContent);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            osw.flush();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        //关闭文件流
-        try {
-            fis.close();
-            isr.close();
-            fos.close();
-            osw.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         }
 
     }
